9e75751c4887d7eba733ed355a4c80d01be28ea2
chrisyhlee <leechr8393@gmail.com>
2023-02-13 17:40:52 -0800

    setup

---
+++ app/javascript/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
