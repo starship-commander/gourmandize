2f6ba32615c1cb7e20efd87f2b16b2e1a18a723a
priceymineral <tudaeva@gmail.com>
2023-02-15 13:25:52 -0800

    New and updated reviews and users are validated to ensure data is adequate and valid specs are provided to test validations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a email' do
+    user = User.create(
+      username: 'commander',
+      password: 'password'
+    )
+      expect(user.errors[:email]).to_not be_empty
+    end
+  it 'is not valid without a username' do
+    user = User.create(
+      email: 'commander@email.com',
+      password: 'password'
+    )
+      expect(user.errors[:username]).to_not be_empty
+  end
+  it 'is not valid without a unique username' do
+    user1 = User.create(
+      username: 'commander',
+      email: 'commander@email.com',
+      password: 'password'
+    )
+    user2 = User.create(
+      username: 'commander',
+      email: 'commander1@email.com',
+      password: 'password'
+    )
+    expect(user2.errors[:username]).to_not be_empty
+  end
+  it 'is not valid without a unique email' do
+    user1 = User.create(
+      username: 'commander',
+      email: 'commander@email.com',
+      password: 'password'
+    )
+    user2 = User.create(
+      username: 'commander1',
+      email: 'commander@email.com',
+      password: 'password'
+    )
+    expect(user2.errors[:email]).to_not be_empty
+  end
 end
