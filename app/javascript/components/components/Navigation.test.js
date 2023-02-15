2e8d72bc74ac4e1819e308902692c746efd11369
nvencer98 <vencer98@gmail.com>
2023-02-15 11:36:58 -0800

    working on navigation tests

--- app/javascript/components/components/Navigation.test.js
+++ app/javascript/components/components/Navigation.test.js
@@ -1,11 +1,38 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Navigation from "./Navigation"
 import { BrowserRouter } from "react-router-dom"
+import userEvent from "@testing-library/user-event"
+
 
 describe("<Navigation />", () => {
   it("renders without crashing", () => {
-    const div = document.createElement("div")
-    render(<Navigation />,div)
+    render(
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
+    )
   })
+  it("has clickable links", () => {
+    render(
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
+    )
+    userEvent.click(screen.getByText("Home"))
+    expect(screen.getByText("Home")).toBeInTheDocument()
+  })
+  
+  it("has a signout link", () => {
+    render(
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>        
+    )
+    screen.logTestingPlaygroundURL()
+    userEvent.click(screen.getByText('Sign Out'))
+    expect(screen.getByRole('link', {name: /sign out/i}).toBeInTheDocument())
+  })
+
+  
 })
