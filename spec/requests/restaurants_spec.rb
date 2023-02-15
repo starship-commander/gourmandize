1d31d71ff81f453e29b0976cb97b2ca0e17c30a2
priceymineral <tudaeva@gmail.com>
2023-02-15 15:16:26 -0800

    added specs for restaurants and reviews. added index method to review and restaurant controllers.

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -2,6 +2,32 @@ require 'rails_helper'
 
 RSpec.describe "Restaurants", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+    it 'gets all restaurants' do
+      user = User.create!(
+        email: 'cheesy_boy@email.com',
+        password: 'cheese',
+        username: 'CheesyBoy'
+      )
+
+      restaurant = Restaurant.create!(
+        name: 'Best Cheeseburger',
+        cuisine: 'Cheeseburger',
+        street: '123 ABC Rd',
+        city: 'Cheeseburgertown',
+        state: 'Cheeseburgerfornia',
+        zip_code: '90210',
+        avg_rating: 4.9,
+        number_of_reviews: 1000,
+        price_range: 1,
+        menu_link: 'best_cheeseburger@cheeseburger.com',
+        images: 'picture_of_cheeseburger.png',
+        user_id: user.id
+      )
+
+      get '/restaurants' 
+      restaurants = JSON.parse(response.body)
+      expect(restaurants.length).to eq 1
+      expect(response).to have_http_status(200)
+    end
+  end  
 end
