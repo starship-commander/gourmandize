fd7e9edd4b9c520371ddbfe4624e7bc0100712cc
chrisyhlee <leechr8393@gmail.com>
2023-02-16 11:09:04 -0800

    update endpoint backend and rspec tests

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe "Reviews", type: :request do
         images: 'picture_of_cheeseburger.png',
         user_id: user.id
       )
-      
+
       review_params = {
         review: {
           meal: 'Cheeseburger',
@@ -79,6 +79,64 @@ RSpec.describe "Reviews", type: :request do
       expect(review.meal).to eq 'Cheeseburger'
       expect(review.rating).to eq 5
       expect(Review.count).to eq 1
+    end
+  end
+
+  describe 'PATCH /update/:id' do
+    it 'will update review' do
+
+      User.create(
+        email: 'cheesy_boy@email.com',
+        password: 'cheese',
+        password_confirmation: 'cheese',
+        username: 'CheesyBoy'
+      )
+      user = User.first
+
+      restaurant = Restaurant.create(
+        name: 'Best Cheeseburger',
+        cuisine: 'Cheeseburger',
+        street: '123 ABC Rd',
+        city: 'Cheeseburgertown',
+        state: 'Cheeseburgerfornia',
+        zip_code: '90210',
+        avg_rating: 4.9,
+        number_of_reviews: 1000,
+        price_range: 1,
+        menu_link: 'best_cheeseburger@cheeseburger.com',
+        images: 'picture_of_cheeseburger.png',
+        user_id: user.id
+      )
+
+      review_params = {
+        review: {
+          meal: 'Cheeseburger',
+          content: 'The cheeseburger was the best I have ever had in my entire life.',
+          rating: 5,
+          user_id: user.id,
+          restaurant_id: restaurant.id
+        }
+      }
+
+      post '/reviews', params: review_params
+      expect(response).to have_http_status(200)
+      review = Review.first
+      expect(review.meal).to eq 'Cheeseburger'
+
+      updated_params = {
+        review: {
+          meal: 'Hamburger',
+          content: 'The hamburger was the worst I have ever had in my entire life. Needs some cheese.',
+          rating: 1,
+          user_id: user.id,
+          restaurant_id: restaurant.id
+        }
+      }
+
+      patch "/reviews/#{review.id}", params: updated_params
+      expect(response).to have_http_status(200)
+      updated_review = Review.first
+      expect(updated_review.meal).to eq 'Hamburger'
     end
   end
 end
