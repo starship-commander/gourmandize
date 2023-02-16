740719596caad83732df06b2af980afb12366759
nvencer98 <vencer98@gmail.com>
2023-02-15 16:30:16 -0800

    working on index

--- app/javascript/components/pages/RestaurantIndex.js
+++ app/javascript/components/pages/RestaurantIndex.js
@@ -1,10 +1,80 @@
 import React from "react";
+import { Card, CardImg, CardText, CardTitle, CardBody, ListGroup, ListGroupItem, CardLink} from "reactstrap";
 
-const RestaurantIndex = () => {
+
+const RestaurantIndex = ({ restaurants }) => {
     return(
-      <>
-        <h1>RestaurantIndex</h1>
-      </>
+      <main>
+        <h1>Restaurants</h1>
+        <div>
+          <Card className="my-2">
+            <CardImg
+              alt="Card image cap"
+              src="https://picsum.photos/900/180"
+              style={{
+                height: 180
+              }}
+              top
+              width="100%"
+            />
+            <CardBody>
+              <CardTitle tag="h5">
+                Todays top pick
+              </CardTitle>
+              <CardText>
+                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+              </CardText>
+              <CardText>
+                <small className="text-muted">
+                  Last updated 3 mins ago
+                </small>
+              </CardText>
+            </CardBody>
+          </Card>
+        </div>
+        
+          {restaurants?.map((restaurant, index) => {
+            return(
+          <Card
+            style={{
+              width: '18rem'
+            }}
+            key = {index}
+          >
+            <img
+              alt="Card"
+              src="https://picsum.photos/300/200"
+            />
+            <CardBody>
+              <CardTitle tag="h5">
+                {restaurant.name}
+              </CardTitle>
+              <CardText>
+                This is some text within a card body.
+              </CardText>
+            </CardBody>
+            <ListGroup flush>
+              <ListGroupItem>
+                An item
+              </ListGroupItem>
+              <ListGroupItem>
+                A second item
+              </ListGroupItem>
+              <ListGroupItem>
+                And a third item
+              </ListGroupItem>
+            </ListGroup>
+            <CardBody>
+              <CardLink href="#">
+                Card Link
+              </CardLink>
+              <CardLink href="#">
+                Another Card Link
+              </CardLink>
+            </CardBody>
+          </Card>
+         ) })}
+      </main>
     )
   }
   
--- app/javascript/components/pages/RestaurantIndex.test.js
+++ app/javascript/components/pages/RestaurantIndex.test.js
@@ -2,10 +2,39 @@ import React from "react"
 import { render } from "@testing-library/react"
 import RestaurantIndex from "./RestaurantIndex"
 import { BrowserRouter } from "react-router-dom"
+import { screen } from "@testing-library/react"
 
 describe("<RestaurantIndex />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
     render(<RestaurantIndex />,div)
   })
+  const restaurants = [{
+    name: 'Best Cheeseburger',
+        cuisine: 'Cheeseburger',
+        street: '123 ABC Rd',
+        city: 'Cheeseburgertown',
+        state: 'Cheeseburgerfornia',
+        zip_code: '90210',
+        avg_rating: 4.9,
+        number_of_reviews: 1000,
+        price_range: 1,
+        menu_link: 'best_cheeseburger@cheeseburger.com',
+        images: 'picture_of_cheeseburger.png',
+        user_id: 1
+  }]
+  it("todays top pick", () => {
+    const div = document.createElement("div")
+    render(<RestaurantIndex restaurants={restaurants}/>,div)
+    screen.logTestingPlaygroundURL()
+    expect(screen.getByRole('heading', {name: /todays top pick/i})).toBeInTheDocument()
+  })
+
+  it("has a card title", () => {
+    const div = document.createElement("div")
+    render(<RestaurantIndex restaurants={restaurants}/>,div)
+    screen.logTestingPlaygroundURL()
+    expect(screen.getByRole('heading', {name: /best cheeseburger/i})).toBeInTheDocument()
+  })
+  
 })
