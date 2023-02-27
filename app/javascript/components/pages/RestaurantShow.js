f8e6ef9e15a3c4d74c3d8fcdf9d147f7fc597521
SammyT13 <sammyt1316@yahoo.com>
2023-02-27 15:25:30 -0800

    completed restaurant API and updated images on RestaurantShow file

--- app/javascript/components/pages/RestaurantShow.js
+++ app/javascript/components/pages/RestaurantShow.js
@@ -25,7 +25,7 @@ const RestaurantShow = ({ restaurants, loggedIn, reviews, currentUser, users }) 
             marginLeft:'5vw'
           }}>
             <div className='res-show'>
-              <img src="https://images.unsplash.com/photo-1551782450-17144efb9c50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" style={{height: '100%', width:'100%', objectFit:'cover'}}/>
+              <img src={currentRestaurant.images} style={{height: '100%', width:'100%', objectFit:'cover'}}/>
             </div>
             <div style={{
               height:'fit-content',
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,43 +7,72 @@ yelp_api_call = Curl.get('https://api.yelp.com/v3/businesses/search?&location=Sa
   http.headers['Authorization'] = "Bearer #{yelp_key}"
 end
 parsed = JSON.parse(yelp_api_call.body_str)
-puts parsed['businesses'].count
-# puts parsed['businesses'].each do |restaurant|
-#   20.times do |id|
-# Restaurant.create!(
-#   name: restaurant['name'],
-#   cuisine: Faker::Restaurant.type,
-#   street: Faker::Address.street_address,
-#   city: Faker::Address.city,
-#   state: Faker::Address.state,
-#   zip_code: Faker::Address.zip_code,
-#   avg_rating: Faker::Number.within(range: 1.0..5.0).round(2),
-#   number_of_reviews: Faker::Number.within(range: 1..5000),
-#   price_range: Faker::Number.within(range: 1..4),
-#   menu_link: Faker::Internet.url,
-#   images: Faker::LoremFlickr.image(search_terms: ['food', 'restaurant', 'restaurants']),
-#   user_id: Faker::Number.within(range:1..10)
-# )
-#   end
-# end
 
-# 10.times do |id|
-#   User.create!(
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password(min_length: 6, max_length: 20),
-#     username: Faker::Internet.username
-#   )
-# end
+10.times do |id|
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6, max_length: 20),
+    username: Faker::Internet.username
+  )
+end
 
+parsed['businesses'].each do |restaurant|
+  Restaurant.create!(
+    name: restaurant['name'],
+    cuisine: restaurant['categories'][0]['title'],
+    street: restaurant['location']['address1'],
+    city: restaurant['location']['city'],
+    state: restaurant['location']['state'],
+    zip_code: restaurant['location']['zip_code'],
+    avg_rating: restaurant['rating'],
+    number_of_reviews: restaurant['review_count'],
+    price_range: Faker::Number.within(range:1..4),
+    menu_link: restaurant['url'],
+    images: restaurant['image_url'],
+    user_id: Faker::Number.within(range:1..10)
+  ) 
+end
 
-# 100.times do |id|
-#   Review.create!(
-#     meal: Faker::Food.dish,
-#     content: Faker::Restaurant.review,
-#     rating: Faker::Number.within(range: 1..5),
-#     user_id: Faker::Number.within(range:1..10),
-#     restaurant_id: Faker::Number.within(range: 1..20),
-#     image: Faker::LoremFlickr.image(search_terms: ['food', 'restaurant'])
-#   )
-# end
+100.times do |id|
+  Review.create!(
+    meal: Faker::Food.dish,
+    content: Faker::Restaurant.review,
+    rating: Faker::Number.within(range: 1..5),
+    user_id: Faker::Number.within(range:1..10),
+    restaurant_id: Faker::Number.within(range: 1..20),
+    image: Faker::LoremFlickr.image(search_terms: ['food', 'restaurant'])
+  )
+end
 
+# Yelp API Model:
+
+# yelp = {
+#   "id"=>"eGhOLzFFIhrvF6QjSSpJsg", 
+#   "alias"=>"phils-bbq-san-diego-2", 
+#   "name"=>"Phil's BBQ", 
+#   "image_url"=>"https://s3-media2.fl.yelpcdn.com/bphoto/IijLAPbkYwMrvcMgNo_8vw/o.jpg", 
+#   "is_closed"=>false, 
+#   "url"=>"https://www.yelp.com/biz/phils-bbq-san-diego-2?adjust_creative=AemLlpZtoX7awyuA4xnLOw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=AemLlpZtoX7awyuA4xnLOw", 
+#   "review_count"=>15751, 
+#   "categories"=>[
+#     {"alias"=>"bbq", "title"=>"Barbeque"}, 
+#     {"alias"=>"bars", "title"=>"Bars"}, 
+#     {"alias"=>"tradamerican", "title"=>"American (Traditional)"}], 
+#   "rating"=>4.5, 
+#   "coordinates"=>{"latitude"=>32.7547792020658, "longitude"=>-117.21598848605}, 
+#   "transactions"=>["delivery"], 
+#   "price"=>"$$", 
+#   "location"=>{
+#     "address1"=>"3750 Sports Arena Blvd", 
+#     "address2"=>"", 
+#     "address3"=>"", 
+#     "city"=>"San Diego", 
+#     "zip_code"=>"92110", 
+#     "country"=>"US", 
+#     "state"=>"CA", 
+#     "display_address"=>["3750 Sports Arena Blvd", "San Diego, CA 92110"]}, 
+#   "phone"=>"+16192266333", 
+#   "display_phone"=>"(619) 226-6333", 
+#   "distance"=>7024.037700535082
+# }
+# puts yelp
