37150fff561dff28f4456096b255879344d9a869
SammyT13 <sammyt1316@yahoo.com>
2023-02-28 09:38:28 -0800

    added restaurant names to mypost page

--- app/javascript/components/pages/MyPosts.js
+++ app/javascript/components/pages/MyPosts.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReviewIndex from "./ReviewIndex"
 import FallingStars from "./FallingStars"
 
-const MyPost = ({ reviews, currentUser, users }) => {
+const MyPost = ({ reviews, currentUser, users, restaurants }) => {
   const filteredReviews = reviews?.filter ((review) => {return review.user_id === currentUser.id})
   
   return(
@@ -10,7 +10,7 @@ const MyPost = ({ reviews, currentUser, users }) => {
       <div className="page-body" style={{textAlign:'center'}}>
         <h1 >My Posts</h1>
       </div>
-        < ReviewIndex reviews={filteredReviews} currentUser={currentUser} users={users} />
+        < ReviewIndex reviews={filteredReviews} currentUser={currentUser} users={users} restaurants={restaurants}/>
       <FallingStars />
     </>
   )
